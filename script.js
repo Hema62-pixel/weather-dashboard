3d4fe8c3357b7b4097fffd908b3505a7713f8622
Hema62-pixel <hemathirumoorthy95@gmail.com>
2026-06-12 15:57:27 +0530

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_KEY = "98a895c36f944c0ff64b0b62c914bac3";
 
 async function getWeather() {
   try {
+    
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=Coimbatore,IN&appid=${API_KEY}&units=metric`
     );
