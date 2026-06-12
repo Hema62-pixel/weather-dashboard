2c1d76be61164536af4af8589cfbd90e9e93598a
Hema62-pixel <hemathirumoorthy95@gmail.com>
2026-06-12 15:30:42 +0530

    script.js

--- script.js
+++ script.js
@@ -1,83 +1,23 @@
-const API_KEY = "https://api.openweathermap.org/data/2.5/weather?q=Coimbatore&appid=YOUR_API_KEY&units=metric";
+const API_KEY = "https://api.openweathermap.org/data/2.5/weather?q=Coimbatore,IN&appid=YOUR_API_KEY&units=metric";
 
-const cityInput = document.getElementById("cityInput");
-const searchBtn = document.getElementById("searchBtn");
+async function getWeather() {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Coimbatore,IN&appid=${API_KEY}&units=metric`
+    );
 
-const weatherCard = document.getElementById("weatherCard");
-const loading = document.getElementById("loading");
-const error = document.getElementById("error");
+    const data = await response.json();
 
-const cityName = document.getElementById("cityName");
-const temperature = document.getElementById("temperature");
-const humidity = document.getElementById("humidity");
-const wind = document.getElementById("wind");
-const condition = document.getElementById("condition");
-
-async function fetchWeather(city) {
-
-    try {
-        error.textContent = "";
-        weatherCard.classList.add("hidden");
-        loading.classList.remove("hidden");
-
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        );
-
-        if (!response.ok) {
-            throw new Error("City not found");
-        }
-
-        const data = await response.json();
-
-        displayWeather(data);
-
-    } catch (err) {
-        error.textContent = err.message;
-    } finally {
-        loading.classList.add("hidden");
+    if (!response.ok) {
+      throw new Error(data.message);
     }
+
+    console.log("Temperature:", data.main.temp);
+    console.log("Humidity:", data.main.humidity);
+    console.log("Wind Speed:", data.wind.speed);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function displayWeather(data) {
-
-    /*
-      Nested JSON Example:
-
-      data.main.temp
-      data.main.humidity
-      data.wind.speed
-      data.weather[0].description
-    */
-
-    cityName.textContent = data.name;
-
-    temperature.textContent = data.main.temp;
-
-    humidity.textContent = data.main.humidity;
-
-    wind.textContent = data.wind.speed;
-
-    condition.textContent =
-        data.weather[0].description;
-
-    weatherCard.classList.remove("hidden");
-}
-
-searchBtn.addEventListener("click", () => {
-
-    const city = cityInput.value.trim();
-
-    if (!city) {
-        error.textContent = "Please enter a city name.";
-        return;
-    }
-
-    fetchWeather(city);
-});
-
-cityInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        searchBtn.click();
-    }
-});
+getWeather();
